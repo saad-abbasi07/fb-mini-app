60b8d0b3eac47df50ffcf56f9242792700300004
saad-abbasi07 <07notsaadabbasi@gmail.com>
2025-08-27 09:36:45 +0500

    update register route

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -6,9 +6,9 @@ import User from "@/models/User";
 export async function POST(req) {
   try {
     await dbConnect();
-    const { name, email, password } = await req.json();
+    const { name, email, password, day, month, year, gender } = await req.json();
 
-    if (!name || !email || !password) {
+    if (!name || !email || !password || !day || !month || !year || !gender) {
       return NextResponse.json({ error: "All fields required" }, { status: 400 });
     }
 
@@ -19,7 +19,7 @@ export async function POST(req) {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await User.create({ name, email, password: hashedPassword });
+    const user = await User.create({ name, email, password: hashedPassword, day, month, year, gender });
 
     return NextResponse.json({ message: "User registered", user: { name: user.name, email: user.email } });
   } catch (err) {
